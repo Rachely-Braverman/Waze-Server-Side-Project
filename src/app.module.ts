4d5b3e566e34ab604c48ddd40ed5217540811156
Rachely-Braverman <rachelb55677@gmail.com>
2022-08-08 11:42:01 +0300

    install cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-
 
 @Module({
   imports: [UserModule, MongooseModule.forRoot('mongodb://localhost:27017')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
