1b05819ac58c40f069910a501f5d3c51d7797fc1
Rachely-Braverman <rachelb55677@gmail.com>
2022-08-31 18:34:17 +0300

    cahnge delete in system

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,13 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { SystemModule } from './system/system.module';
-import { LocationController } from './location/location.controller';
 import { LocationModule } from './location/location.module';
-
-@Module({
-  imports: [UserModule, SystemModule, LocationModule, MongooseModule.forRoot('mongodb://localhost:27017')],
 import { ManagerModule } from './manager/manager.module';
 import { RequestModule } from './request/request.module';
 
 @Module({
   imports: [UserModule, SystemModule, MongooseModule.forRoot('mongodb://localhost:27017'), 
-  ManagerModule, RequestModule],
+  ManagerModule, RequestModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
